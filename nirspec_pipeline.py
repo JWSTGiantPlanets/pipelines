00c3740bc064f0cd7a767614d1dda11066509f5b
Oliver King <oliver.king95@gmail.com>
2023-05-04 13:22:44 +0100

    Create nirspec_pipeline.py

---
+++ nirspec_pipeline.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+NIRSPEC pipeline
+
+TODO
+- everything
+"""
+
+import sys
+
+
+def main(*args):
+    pass
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
