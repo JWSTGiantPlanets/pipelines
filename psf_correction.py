089c9ef13fa2b047e1740ec6d9397bd6980cd5e0
Oliver King <oliver.king95@gmail.com>
2023-09-13 11:58:13 +0100

    Upscale forward model to increase PSF correction

--- psf_correction.py
+++ psf_correction.py
@@ -1,9 +1,14 @@
+from typing import Callable, TypeAlias
+
 import numpy as np
 import planetmapper
 from astropy.io import fits
 
+# Transform from original to scaled pixel coordinates:
+CoordinateTransform: TypeAlias = Callable[[int], int]
 
-def forward_model(path: str) -> np.ndarray:
+
+def forward_model(body: planetmapper.BodyXY) -> np.ndarray:
     """
     Get forward model image for an observation.
 
@@ -11,16 +16,51 @@ def forward_model(path: str) -> np.ndarray:
     value of 0.
 
     Args:
-        path: Path to observation FITS file.
+        body: planetmapper.BodyXY object to forward model.
 
     Returns:
         Numpy array containing forward model image.
     """
-    # TODO need to expand the image to include full planet rather than just the FOV
-    # TODO probably want to oversample the image to model the PSF better
-    observation = planetmapper.Observation(path)
     return np.asarray(
-        (observation.get_emission_angle_img() < 90)
-        & (observation.get_incidence_angle_img() < 90),
+        (body.get_emission_angle_img() < 90) & (body.get_incidence_angle_img() < 90),
         dtype=float,
     )
+
+
+def create_scaled_body(
+    observation: planetmapper.Observation,
+    resize: int,
+    border: int,
+) -> tuple[planetmapper.BodyXY, CoordinateTransform]:
+    """
+    Create a scaled version of the input observation to oversample the pixel grid when
+    forward modelling.
+
+    Args:
+        observation: Observation to scale.
+        resize: Scaling factor to resize the image by.
+        border: Number of pixels to add to the border of the image after rescaling.
+
+    Returns:
+        `(body, transform)` tuple where `body` is the scaled version of the input
+        observation and `transform` is a function that can be used to transform
+        pixel coordinates from the input observation to the scaled version.
+    """
+    body = planetmapper.BodyXY.from_body(observation.to_body())
+
+    nx, ny = observation.get_img_size()
+    body.set_img_size(nx * resize + border * 2, ny * resize + border * 2)
+    body.set_r0(observation.get_r0() * resize)
+    body.set_x0(observation.get_x0() * resize + border)
+    body.set_y0(observation.get_y0() * resize + border)
+    body.set_rotation(observation.get_rotation())
+
+    transform: CoordinateTransform = lambda x: x * resize + border
+    return body, transform
+
+
+def create_dynamically_scaled_body(
+    observation: planetmapper.Observation,
+) -> tuple[planetmapper.BodyXY, CoordinateTransform]:
+    # TODO generate scaled body using appropriate values for resize & border
+    pass
